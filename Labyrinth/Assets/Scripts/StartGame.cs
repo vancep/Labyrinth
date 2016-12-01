eca1c7007fd819875b18d50d9204827d79b8eb8e
Vance <vancepiscitelli@gmail.com>
2016-12-01 10:27:59 -0800

    Changed loading level to Async. Added some texture to the marble.

--- Labyrinth/Assets/Scripts/StartGame.cs
+++ Labyrinth/Assets/Scripts/StartGame.cs
@@ -7,21 +7,23 @@ public class StartGame : MonoBehaviour
 	public string startScene;
 
 	private Scene scene;
+	//private AsyncOperation loadLevel; // might want in future
 
 	// Use this for initialization
 	void Start () 
 	{
 		Debug.Log("Scene Count:" + SceneManager.sceneCount);
 
-		SceneManager.LoadScene(startScene);
+		SceneManager.LoadSceneAsync(startScene); // could assign a variable to this if necessary
 
 		scene = SceneManager.GetSceneByName(startScene);
+
 		Debug.Log(scene.name);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+
 	}
 }
