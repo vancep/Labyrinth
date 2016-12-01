097f52373e288408ca4d7f23896be8f7212d8d87
Vance <vancepiscitelli@gmail.com>
2016-12-01 10:02:15 -0800

    Added start screen as a separate scene. Should switch to use Async.

---
+++ Labyrinth/Assets/Scripts/StartGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartGame : MonoBehaviour 
+{
+	public string startScene;
+
+	private Scene scene;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log("Scene Count:" + SceneManager.sceneCount);
+
+		SceneManager.LoadScene(startScene);
+
+		scene = SceneManager.GetSceneByName(startScene);
+		Debug.Log(scene.name);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
