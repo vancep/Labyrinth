a0eb9c0847d79110eac915691318cb5a64ccf9cc
Vance <vancepiscitelli@gmail.com>
2016-12-04 13:36:17 -0800

    Added Ads when game is played as an android app. muahahaa!

---
+++ Labyrinth/Assets/Scripts/AdScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ShowAd();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowAd()
+	{
+		Debug.Log("Attempting to Show Ad");
+
+		if(Advertisement.IsReady())
+		{
+			Debug.Log("Showing Ad");
+			Advertisement.Show();
+		}
+		else
+		{
+			Debug.Log("Not Showing Ad");
+		}
+	}
+}
