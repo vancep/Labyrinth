6f5044c584103d360270e2ff6966e00f2ec74fc1
Vance <vancepiscitelli@gmail.com>
2016-12-04 17:26:42 -0800

    Added sound when marble collides with blocks. Just need to make some limits on it.

--- Labyrinth/Assets/Scripts/PlayerController.cs
+++ Labyrinth/Assets/Scripts/PlayerController.cs
@@ -9,15 +9,26 @@ public class PlayerController : MonoBehaviour {
 
 	private bool atEnd = false;
 
+	private AudioSource audioSource;
+	private float previousMag;
+	private float mag;
+
 	// Use this for initialization
 	void Start () 
 	{
+		audioSource = GetComponent<AudioSource>();
 		rb = GetComponent<Rigidbody>();
+
+		previousMag = mag = rb.velocity.magnitude;
 	}
 
 	void FixedUpdate()
 	{
+		// used for sound when crashing
+		previousMag = mag;
+		mag = rb.velocity.magnitude;
 
+		// for mouse controls
 		if(Input.GetButton("Fire1"))
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -47,9 +58,27 @@ public class PlayerController : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
+		// marble reached the end of the maze
 		if(other.gameObject.CompareTag("End"))
 		{
 			atEnd = true;
+		}
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		// for noise when marble hits another object
+		if(other.gameObject.CompareTag("Block"))
+		{
+			// for now, if the marble collides into a block at a specific height, play a sound.
+			// since the floors and walls are the same objects, cant just play the sound when the marble collides with any block.
+			if(other.gameObject.transform.position.y >= 0.4)
+			{
+				audioSource.volume = Mathf.Min(1.0f, previousMag/8);
+				Debug.Log("Vol: " + audioSource.volume);
+				audioSource.Play();
+			}
 		}
 	}
 
